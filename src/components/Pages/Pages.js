96ad877aad5d3ad158947d9a918e4d0f3f64ace8
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-10-08 14:12:12 +0300

    Апнул блок с страницами, теперь они в виде DOCK

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -2,18 +2,16 @@ import React  from "react";
 import styles from "./Pages.module.css";
 import Page from "./Page/Page";
 
-const Pages = ({ count, current }) => {
-  return (
-    <ul className={ styles.pages }>
-      { new Array(count).fill(``).map((_, i) => {
-        const number = i + 1;
+const Pages = ({ count, current }) => (
+  <ul className={ styles.pages }>
+    { new Array(count).fill(``).map((_, i) => {
+      const number = i + 1;
 
-        return (
-          <Page key={ number } current={ number === current }>{ number }</Page>
-        );
-      }) }
-    </ul>
-  );
-};
+      return (
+        <Page key={ number } current={ number === current }>{ number }</Page>
+      );
+    }) }
+  </ul>
+);
 
 export default Pages;
