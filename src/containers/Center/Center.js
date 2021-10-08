ee6184daa2884af960c1b9bc7476bed01c4add1f
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-10-08 14:44:46 +0300

    Добавил контейнер центрующий дочерние компоненты

---
+++ src/containers/Center/Center.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Center.module.css";
+
+const Center = ({ children }) => (
+  <div className={ styles.center }>
+    { children }
+  </div>
+);
+
+export default Center;
