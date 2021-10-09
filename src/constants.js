d897949f7a61ebb2d022b2d3aa7d922dcdf75b25
AlexKomz <alexander.9551@yahoo.com>
2021-10-09 20:08:00 +0300

    Добавил файл констант

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const UNKNOWN_VALUE = `unknown`;
+export const EMPTY_VALUE = ``;
+
+export const Label = {
+  NAME: `Name`,
+  STATUS: `Status`,
+  SPECIES: `Species`,
+  TYPE: `Type`,
+  GENDER: `Gender`,
+  ORIGIN: `Origin`,
+  LOCATION: `Location`,
+  EPISODES: `Episodes`,
+  CREATED: `Created`
+};
+
+export const ButtonType = {
+  SUBMIT: `submit`,
+  BUTTON: `button`
+};
+
+export const InputType = {
+  TEXT: `text`
+};
+
+export const Size = {
+  SMALL: `small`,
+  LARGE: `large`,
+  DEFAULT: `default`
+};
+
+export const Theme = {
+  DARK: `dark`,
+  LIGHT: `light`,
+  TRANSPARENT: `transparent`
+};
