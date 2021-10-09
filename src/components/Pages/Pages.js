553fce08a25ffd4f77924ff9cbb0358471bd49c6
AlexKomz <alexander.9551@yahoo.com>
2021-10-09 20:39:49 +0300

    Переделал отрисовку кнопок страниц на более оптимальную/сокращенную форму

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -1,6 +1,27 @@
-import React  from "react";
+import React, { Fragment } from "react";
 import styles from "./Pages.module.css";
 import Page from "./Page/Page";
+
+const FIRST = 1;
+const MAX_INCREMENT = 2;
+
+const ExtremePageFragment = ({ firstPart, secondPart }) => (
+  <Fragment>
+    { firstPart }
+    { secondPart }
+  </Fragment>
+);
+
+const extremePagesRender = (number, count) => {
+  const page = <Page current={ false }>{ number }</Page>;
+  const gap = <li className={ styles.gap }>...</li>;
+
+  if (number === FIRST) {
+    return <ExtremePageFragment key={ number } firstPart={ page } secondPart={ gap } />;
+  } else if (number === count) {
+    return <ExtremePageFragment key={ number } firstPart={ gap } secondPart={ page } />;
+  }
+};
 
 const Pages = ({ count, current }) => (
   <ul className={ styles.pages }>
@@ -8,7 +29,9 @@ const Pages = ({ count, current }) => (
       const number = i + 1;
 
       return (
-        <Page key={ number } current={ number === current }>{ number }</Page>
+        Math.abs(number - current) > MAX_INCREMENT
+          ? extremePagesRender(number, count)
+          : <Page key={ number } current={ number === current }>{ number }</Page>
       );
     }) }
   </ul>
